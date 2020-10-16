6447a589e1b8d4e6247d3b85d8c1b721a641a9ef
amiedawn <amie.d.carroll@gmail.com>
2020-10-16 01:15:21 -0600

    view high scores links to scores page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,28 @@
 var timerEl = document.getElementById('countdown');
 
+
+
 //timer that starts at 60 seconds
 function countdown() {
   var timeLeft = 60;
+  console.log(timeLeft);
   // use setInterval() method to call a function every time we want the timer
   var timeInterval = setInterval(function() {
     if (timeLeft >= 1) {
-      timerEl.textContent = timeLeft;
+      parseInt(timerEl.textContent) = timeLeft;
       timeLeft= timeLeft - 1;
     } else if (timeLeft === 0) {
-        timerEl.textContent = '';
+        parseInt(timerEl.textContent) = '';
         clearInterval(timeInterval);
-        // displayMessage(); call the function here that will take you to Summary Page
+        // invokeSummaryPage(); call the function here that will take you to Summary Page
       }
   }, 3000);
 }  
+
+//function invokeSummaryPage() { // <== this function takes you to summary and displays your final score
+
+
+
+//}
+
+countdown();
