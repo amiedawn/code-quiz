a70145821e04db17618a2f84e586524f1a212bd6
amiedawn <amie.d.carroll@gmail.com>
2020-10-17 09:17:16 -0600

    save to get help on error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,88 +1,163 @@
+var start = document.querySelector("#startQuiz");
+var frameTotal = document.querySelector("#frameTotal");
+var showQuestion = document.querySelector("#showQuestion");
+var showTimeGauge = document.querySelector("#showTimeGauge");
+var choiceA = document.querySelector("#A");
+var choiceB = document.querySelector("#B");
+var choiceC = document.querySelector("#C");
+var choiceD = document.querySelector("#D");
+var showCounter = document.querySelector("#showCounter");
+var progress = document.querySelector("#progress");
+var showRightOrWrong = document.querySelector("#showRightOrWrong");
+var highScores = document.querySelector(highScores);
+var showStatus = document.querySelector(showStatus);
+
+//debugger;
+ // display questions
+ var arrQuestions = [
+  {
+    showQuestion: "1.  Commonly used data types DO NOT include: ",
+    choiceA: "a.  strings",
+    choiceB: "b.  booleans",
+    choiceC: "c.  alerts",
+    choiceD: "d.  numbers",
+    rightChoice: "C"
+  },
+  {
+    showQuestion: "2.  A very useful tool during development and debugging for printing content to the debugger is: ",
+    choiceA: "a.  JavaScript",
+    choiceB: "b.  terminal/bash",
+    choiceC: "c.  for loops",
+    choiceD: "d.  console.log",
+    rightChoice: "D"
+  },
+  {
+    showQuestion: "3.  String values must be enclosed within _____________ when being assigned to variables. ",
+    choiceA: "a.  quotes",
+    choiceB: "b.  curly brackets",
+    choiceC: "c.  parentheses",
+    choiceD: "d.  square brackets",
+    rightChoice: "A"
+  },
+  {
+    showQuestion: "4.  Arrays in JavaScript can be used to store _____________ .",
+    choiceA: "a.  numbers and strings",
+    choiceB: "b.  other arrays",
+    choiceC: "c.  booleans",
+    choiceD: "d.  all of the above",
+    rightChoice: "D"
+  },
+  {
+    showQuestion: "5.  The condition in an if/else statement is enclosed with __________ .",
+    choiceA: "a.  quotes",
+    choiceB: "b.  curly brackets",
+    choiceC: "c.  parentheses",
+    choiceD: "d.  square brackets",
+    rightChoice: "C"
+  }
+];
+
+// global variables
+var lastQuestionIndex = arrQuestions.length - 1;
+var runningQuestionIndex = 0;
+//var timer; // possibly take away var timer in function countdownTimer()
+var rightOrWrong = 0;
+var choice = "";
+var status = "";
+
+function startQuiz() {
+  
+  //var timer = 0;
+  writeQuestions();
+ // generateQuestions();
+  
+ // countdownTimer();
+}
 
 // timer function that starts at 60 and ends at 0
 function countdownTimer() {
-  
-var timeleft = 60;
-var timer = setInterval(function () {
-  timeleft = timeleft - 1;
-  document.getElementById("countdownTimer").textContent = timeleft;
-  if (timeleft <= 0) {
-    clearInterval(timer);
-  }
-}, 1000);  
-};
-
-function writeQuestions() {
-  // display questions
-  generateQuestions();
-};
-
-function generateQuestions() {
-  //loop through questions until timer goes down to 0
-  var arrQuestions = [
-    {
-      selectQuestion: "1.  Commonly used data types DO NOT include: ",
-      choiceA: "a.  strings",
-      choiceB: "b.  booleans",
-      choiceC: "c.  alerts", 
-      choiceD: "d.  numbers",
-      rightChoice: "C"
-    },
-    {
-      selectQuestion: "2.  A very useful tool during development and debugging for printing content to the debugger is: ",
-      choiceA: "a.  JavaScript", 
-      choiceB: "b.  terminal/bash",
-      choiceC: "c.  for loops",
-      choiceD: "d.  console.log", 
-      rightChoice: "D"
-    },
-    {
-      selectQuestion: "3.  String values must be enclosed within _____________ when being assigned to variables. ",
-      choiceA: "a.  quotes",
-      choiceB: "b.  curly brackets",
-      choiceC: "c.  parentheses",
-      choiceD: "d.  square brackets",
-      rightChoice: "A"
-    },
-    {
-      selectQuestion: "4.  Arrays in JavaScript can be used to store _____________ .",
-      choiceA: "a.  numbers and strings",
-      choiceB: "b.  other arrays",
-      choiceC: "c.  booleans",
-      choiceD: "d.  all of the above",
-      rightChoice: "D"
-    },
-    {
-      selectQuestion: "5.  The condition in an if/else statement is enclosed with __________ .", 
-      choiceA: "a.  quotes", 
-      choiceB: "b.  curly brackets", 
-      choiceC: "c.  parentheses",
-      choiceD: "d.  square brackets", 
-      rightChoice: "C"
+  var timeleft = 60;
+  var timer = setInterval(function () {
+    timeleft = timeleft - 1;
+   //**put this back in to get timer to work */
+    // document.getElementById("countdownTimer").textContent = timeleft;
+    if (timeleft <= 0) {
+      clearInterval(timer);
     }
-  ];
-
-  var right = 0;
-//instead of for loop: the pop will take off an answer
-  function displayQuestion() {
-  var question = arrQuestions.pop();
-  if (question) {
-    var answer = confirm(question.selectQuestion);
-    if (answer === question.selectAnswers) {
-      // answer is right
-      //print right at bottom of the screen
-      right = right + 1;
-    } else {
-      // answer is wrong
-      //print wrong at bottom of the screen
-
-    }  
-  }
-}
-
-  
+  }, 1000);  
 };
 
+var generateBtn = document.querySelector("#writeQuestions");
+
+//display question and choices to the screen
+function writeQuestions() {
+  console.log("runningQuestionIndex", runningQuestionIndex);
+  console.log("lastQuestionIndex", lastQuestionIndex);
+  var q = arrQuestions[runningQuestionIndex];
+
+  showQuestion.innerHTML = "<p>" + q.showQuestion + "</p>";
+  choiceA.innerHTML = q.choiceA;
+  choiceB.innerHTML = q.choiceB;
+  choiceC.innerHTML = q.choiceC;
+  choiceD.innerHTML = q.choiceD;
+}  
+//generateQuestions();  
+//writeQuestions(); 
+
+// function generateQuestions() {
+// //loop through questions until timer goes down to 0
+//   for(var i=0; i <= lastQuestionIndex; i++) {
+//     progress.innerHTML = progress.innerHTML + "<div class='prog' id=" + i + "></div>";
+//   }
+// }
+
+
+// function rightAnswer() {
+//   document.getElementById("choice").style.backgroundColor = "green";
+// }
+
+// function wrongAnswer() {
+//   document.getElementById("choice").style.backgroundColor = "red";
+// }
+
+// function showScores() {
+//   // navigate to summary page and display score there
+//   highScores.style.display = "block";
+  
+//*******figure this out */
+
+
+// }
+
+function checkAnswer(answer) {
+  //if (arrQuestions[runningQuestionIndex].rightChoice === answer) {
+  //  status = "Right!"
+    //status.innerHTML = status.innerHTML + "<div class='prog' id=" + i + "></div>";
+    //rightOrWrong = rightOrWrong + 1;
+ // } else {
+   // status = "Wrong!"
+ // }
+  console.log("runningQuestionIndex", runningQuestionIndex);
+  console.log("lastQuestionIndex", lastQuestionIndex);
+  if(runningQuestionIndex < lastQuestionIndex) {
+   // count = 0; // maybe don't need this??
+    writeQuestions();
+    runningQuestionIndex = runningQuestionIndex + 1; //makes it go to the next question
+  } else {
+    showScores();
+  }
+};
+
+// function RightOrWrong() {
+//   if (status === "Right!")
+    
+
+// }
+
+
+
+
+  
 
 
 
@@ -90,13 +165,18 @@ function generateQuestions() {
 
 
 
-// maybe needs to go after event listeners?
-displayQuestion();
-countdownTimer();
+
+
+
 
 // event listeners
+document.addEventListener("click", startQuiz);
 
-generateBtn.addEventlistener("click", writeQuestions);
+//generateBtn.addEventlistener("click", writeQuestions);
 // what you would use if in <body> you have <button id="btnNext">Next</button>
 // this will display the next question
-//document.getElementById("btnNext").addEventListener("click", displayQuestion);
+document.addEventListener("click", checkAnswer());
+
+// maybe needs to go after event listeners?
+//displayQuestion();
+//countdownTimer();
