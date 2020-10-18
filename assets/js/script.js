bef2ff2e40befce895af08d64d5276b2a490cc72
amiedawn <amie.d.carroll@gmail.com>
2020-10-18 08:51:34 -0600

    added init function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,80 +1,84 @@
-var start = document.querySelector("#startQuiz");
-var frameTotal = document.querySelector("#frameTotal");
-var showQuestion = document.querySelector("#showQuestion");
-var showTimer = document.querySelector("#showTimer");
-var totalTimeTrack = document.querySelector("#totalTimeTrack");
-var showTimeTrack = document.querySelector("#showTimeTrack");
-var choiceA = document.querySelector("#A");
-var choiceB = document.querySelector("#B");
-var choiceC = document.querySelector("#C");
-var choiceD = document.querySelector("#D");
-var showCounter = document.querySelector("#showCounter");
-var displayRW = document.querySelector("#displayRW");
-var highScores = document.querySelector(highScores);
-var generateBtn = document.querySelector("#writeQuestions");
-var answerChosen = document.querySelector(".choice")
-
 var currentQIndex = 0;
 var choice = "";
 var timeLeft = 60;
 var timer = 0; 
 
-// Questions and answers:
-var arrQuestions = [];
-arrQuestions = [
-  {
-    showQuestion: "1.  Commonly used data types DO NOT include: ",
-    choiceA: "a.  strings",
-    choiceB: "b.  booleans",
-    choiceC: "c.  alerts",
-    choiceD: "d.  numbers",
-    rightChoice: "c. alerts",
-  },
-  {
-    showQuestion: "2.  A very useful tool during development and debugging for printing content to the debugger is: ",
-    choiceA: "a.  JavaScript",
-    choiceB: "b.  terminal/bash",
-    choiceC: "c.  for loops",
-    choiceD: "d.  console.log",
-    rightChoice: "d. console.log",
-  },
-  {
-    showQuestion: "3.  String values must be enclosed within _____________ when being assigned to variables. ",
-    choiceA: "a.  quotes",
-    choiceB: "b.  curly brackets",
-    choiceC: "c.  parentheses",
-    choiceD: "d.  square brackets",
-    rightChoice: "a. quotes",
-  },
-  {
-    showQuestion: "4.  Arrays in JavaScript can be used to store _____________ .",
-    choiceA: "a.  numbers and strings",
-    choiceB: "b.  other arrays",
-    choiceC: "c.  booleans",
-    choiceD: "d.  all of the above",
-    rightChoice: "d. all of the above",
-  },
-  {
-    showQuestion: "5.  The condition in an if/else statement is enclosed with __________ .",
-    choiceA: "a.  quotes",
-    choiceB: "b.  curly brackets",
-    choiceC: "c.  parentheses",
-    choiceD: "d.  square brackets",
-    rightChoice: "c. parentheses",
-  }
-];
-
-//collect answers chosen by user click
-choiceA.onclick = answerRW;
-choiceB.onclick = answerRW;
-choiceC.onclick = answerRW;
-choiceD.onclick = answerRW;
-
-var lastQIndex = arrQuestions.length - 1;
-
 function startQuiz() {
+  init();
   writeQuestions();
-  showTimer = setInterval(countdownTimer, 1000); 
+  showTimer = setInterval(countdownTimer, 1000);
+}
+
+function init() {
+  var start = document.querySelector("#startQuiz");
+  var frameTotal = document.querySelector("#frameTotal");
+  var showQuestion = document.querySelector("#showQuestion");
+  var showTimer = document.querySelector("#showTimer");
+  var totalTimeTrack = document.querySelector("#totalTimeTrack");
+  var showTimeTrack = document.querySelector("#showTimeTrack");
+  var choiceA = document.querySelector("#A");
+  var choiceB = document.querySelector("#B");
+  var choiceC = document.querySelector("#C");
+  var choiceD = document.querySelector("#D");
+  var showCounter = document.querySelector("#showCounter");
+  var displayRW = document.querySelector("#displayRW");
+  var highScores = document.querySelector(highScores);
+  var generateBtn = document.querySelector("#writeQuestions");
+  var answerChosen = document.querySelector(".choice")
+
+  // Questions and answers:
+  var arrQuestions = [];
+  arrQuestions = [
+    {
+      showQuestion: "1.  Commonly used data types DO NOT include: ",
+      choiceA: "a.  strings",
+      choiceB: "b.  booleans",
+      choiceC: "c.  alerts",
+      choiceD: "d.  numbers",
+      rightChoice: "c. alerts",
+    },
+    {
+      showQuestion: "2.  A very useful tool during development and debugging for printing content to the debugger is: ",
+      choiceA: "a.  JavaScript",
+      choiceB: "b.  terminal/bash",
+      choiceC: "c.  for loops",
+      choiceD: "d.  console.log",
+      rightChoice: "d. console.log",
+    },
+    {
+      showQuestion: "3.  String values must be enclosed within _____________ when being assigned to variables. ",
+      choiceA: "a.  quotes",
+      choiceB: "b.  curly brackets",
+      choiceC: "c.  parentheses",
+      choiceD: "d.  square brackets",
+      rightChoice: "a. quotes",
+    },
+    {
+      showQuestion: "4.  Arrays in JavaScript can be used to store _____________ .",
+      choiceA: "a.  numbers and strings",
+      choiceB: "b.  other arrays",
+      choiceC: "c.  booleans",
+      choiceD: "d.  all of the above",
+      rightChoice: "d. all of the above",
+    },
+    {
+      showQuestion: "5.  The condition in an if/else statement is enclosed with __________ .",
+      choiceA: "a.  quotes",
+      choiceB: "b.  curly brackets",
+      choiceC: "c.  parentheses",
+      choiceD: "d.  square brackets",
+      rightChoice: "c. parentheses",
+    }
+  ];
+
+  //collect answers chosen by user click
+  choiceA.onclick = answerRW;
+  choiceB.onclick = answerRW;
+  choiceC.onclick = answerRW;
+  choiceD.onclick = answerRW;
+
+  var lastQIndex = arrQuestions.length - 1;
+
 }
 
 function countdownTimer() { //print timer to screen
@@ -154,11 +158,11 @@ function showScores() {
 
 
 // event listeners:
-debugger;
+//debugger;
 //document.addEventListener("click", startQuiz);// had before. lines up answer with question, but wont load first question
   //**above, timer shows up after you click but totally wacky
 //start.addEventListener("click", startQuiz); //from video reference (same as next try)
 //start.addEventListener("click", startQuiz);// won't load 1st question, if you click correct answer 1st, it says wrong and then starts with 1, but answers are misaligned
-//document.getElementById("startButton").addEventListener("click", startQuiz); //won't load 1st question, if you click correct answer 1st, it says wrong and then starts with 1, but answers are misaligned
+document.getElementById("startButton").addEventListener("click", startQuiz); //won't load 1st question, if you click correct answer 1st, it says wrong and then starts with 1, but answers are misaligned
 //start.querySelector("startButton").onclick = startQuiz; //won't load 1st question, if you click correct answer 1st, it says wrong and then starts with 1, but answers are misaligned
 //generateBtn.addEventListener("click", writeQuestions);// won't load 1st question, if you click correct answer 1st, it says wrong and then starts with 1, but answers are misaligned
