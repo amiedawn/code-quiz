d704af5e68b17fa22ee5afbdee99f7f9a4b56406
amiedawn <amie.d.carroll@gmail.com>
2020-10-18 06:58:15 -0600

    first question won't load and then index is off

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 var start = document.querySelector("#startQuiz");
 var frameTotal = document.querySelector("#frameTotal");
 var showQuestion = document.querySelector("#showQuestion");
+var showTimer = document.querySelector("#showTimer");
+var totalTimeTrack = document.querySelector("#totalTimeTrack");
 var showTimeTrack = document.querySelector("#showTimeTrack");
 var choiceA = document.querySelector("#A");
 var choiceB = document.querySelector("#B");
@@ -15,10 +17,11 @@ var answerChosen = document.querySelector(".choice")
 var currentQIndex = 0;
 var choice = "";
 var timeLeft = 60;
-var timerEl = "";
+var timer = 0; 
 
 // Questions and answers:
-var arrQuestions = [
+var arrQuestions = [];
+arrQuestions = [
   {
     showQuestion: "1.  Commonly used data types DO NOT include: ",
     choiceA: "a.  strings",
@@ -71,20 +74,33 @@ var lastQIndex = arrQuestions.length - 1;
 
 function startQuiz() {
   writeQuestions();
-  countdownTimer();
+  showTimer = setInterval(countdownTimer, 1000); 
 }
 
-// timer function that starts at 60 and ends at 0
-function countdownTimer() {
-  var timeleft = 60;
-  var timer = setInterval(function () {
-    timeleft = timeleft - 1;
-    document.getElementById("countdownTimer").innerHTML = timeleft;
-    if (timeleft <= 0) {
-      clearInterval(timer);
-    }
-  }, 1000);
+function countdownTimer() { //print timer to screen
+  var x = 0;
+  if (x <= timeLeft) {
+    //counter.innerHTML = x; this is given but doesn't work
+    document.getElementById("countdownTimer").innerHTML = x;
+    x = x - 1;
+  } else {
+    x = 0;
+    //**store their score */
+    checkAnswer();
+  }
 };
+
+// timer function that starts at 60 and ends at 0
+// function countdownTimer() {
+//   //var timeleft = 60;
+//   var timer = setInterval(function () {
+//     timeleft = timeleft - 1;
+//     document.getElementById("countdownTimer").innerHTML = timeleft;
+//     if (timeleft <= 0) {
+//       clearInterval(timer);
+//     }
+//   }, 1000);
+// };
 
 //document.getElementById("countdownTimer").innerHTML = timeLeft;
 //debugger;
@@ -94,7 +110,7 @@ function writeQuestions() {
   console.log("currentQIndex", currentQIndex);
   console.log("lastQIndex", lastQIndex);
   var q = arrQuestions[currentQIndex];
-  
+
   showQuestion.innerHTML = "<p>" + q.showQuestion + "</p>";
   choiceA.innerHTML = q.choiceA;
   choiceB.innerHTML = q.choiceB;
@@ -105,48 +121,51 @@ function writeQuestions() {
 function answerRW(answer) {
   console.log(this.innerText.trim())
   console.log(arrQuestions[currentQIndex].rightChoice.trim())
-  
+
   if (arrQuestions[currentQIndex].rightChoice.trim() === this.innerText.trim()) {
     displayRW.innerHTML = "Right!";
     checkAnswer();
-    
+
   } else {
     displayRW.innerHTML = "Wrong!";
-    timeLeft = timeLeft -10;
+    timeLeft = timeLeft - 10;
     if (timeLeft < 0) {
-    //  timeLeft.innerHTML = "Time: 0 seconds remaining"; doesn't work
-     
+      //  timeLeft.innerHTML = "Time: 0 seconds remaining"; doesn't work
+
     }
     checkAnswer();
   }
 }
 
-//function checkAnswer(answer) {
-  console.log("currentQIndex", currentQIndex);
-  console.log("lastQIndex", lastQIndex);
-  if (currentQIndex < lastQIndex) {
-    writeQuestions();
-    //checkAnswer();
-    currentQIndex = currentQIndex + 1; //makes it go to the next question
-    //displayRW.innerHTML = ""; //??
-  } else {
-    showScores();
-  }
-
+function checkAnswer(answer) {
+console.log("currentQIndex", currentQIndex);
+console.log("lastQIndex", lastQIndex);
+if (currentQIndex < lastQIndex) {
+  writeQuestions();
+  //checkAnswer();
+  currentQIndex = currentQIndex + 1; //makes it go to the next question
+  //displayRW.innerHTML = ""; //??
+} else {
+  clearInterval(showTimer);
+  showScores();
+}
+};
 
 function showScores() {
   // navigate to summary page and display score there
   window.location.href = "summary.html";
 
-/********figure this out */
+  /********figure this out */
 
 
 }
 
 
 // event listeners:
-document.addEventListener("click", startQuiz);
-//document.addEventListener("click", )
+debugger;
+//document.addEventListener("click", startQuiz);// had before. lines up answer with question, but wont load first question
+//start.addEventListener("click", startQuiz); from video reference (same as next try)
+//start.addEventListener("click", startQuiz);// won't load 1st question, if you click correct answer 1st, it says wrong and then starts with 1, but answers are misaligned
+//document.getElementById("startButton").addEventListener("click", startQuiz); //won't load 1st question, if you click correct answer 1st, it says wrong and then starts with 1, but answers are misaligned
+//start.querySelector("startButton").onclick = startQuiz; //won't load 1st question, if you click correct answer 1st, it says wrong and then starts with 1, but answers are misaligned
 
-writeQuestions();
-countdownTimer();
